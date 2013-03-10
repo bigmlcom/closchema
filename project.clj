75ea8f9e17e87e26f1fe198e8011983e2a0f7fae
Jose Antonio Ortega Ruiz <jao@gnu.org>
2013-03-10 20:50:43 +0100

    Clojure version bumped to 1.5.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bigml/closchema "0.3.0"
+(defproject bigml/closchema "0.3.1"
   :description "Implements the JSON Schema specification."
   :dependencies [[cheshire "5.0.1"]
-                 [org.clojure/clojure "1.5.0"]])
+                 [org.clojure/clojure "1.5.1"]])
