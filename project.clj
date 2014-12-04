8866c0034678501dbe96c1747284fe7c4551ef6b
jao <jao@gnu.org>
2014-12-04 01:59:32 +0100

    License and project URL in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject bigml/closchema "0.6-SNAPSHOT"
   :description "Implements the JSON Schema specification."
+  :license {:name "MIT License" :url "http://opensource.org/licenses/MIT"}
+  :url "https://github.com/bigmlcom/closchema"
   :dependencies [[cheshire "5.3.1"]
                  [org.clojure/clojure "1.5.1"]])
