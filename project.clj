3783c4396823052030ff7df405a1f3b728e48721
Chee Sing Lee <lee@bigml.com>
2015-06-26 12:11:23 -0700

    Update cheshire dep -> 5.5.0
    Signed-off-by: Chee Sing Lee <lee@bigml.com>

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Implements the JSON Schema specification."
   :license {:name "MIT License" :url "http://opensource.org/licenses/MIT"}
   :url "https://github.com/bigmlcom/closchema"
-  :dependencies [[cheshire "5.3.1"]
+  :dependencies [[cheshire "5.5.0"]
                  [org.clojure/clojure "1.6.0"]])
